3f2467453b5b22e7661dbae83dbac718485c3d13
acevif <acevif@gmail.com>
2023-01-10 06:08:20 +0900

    Release ignore 0.2.1

--- Formula/ignore.rb
+++ Formula/ignore.rb
@@ -1,15 +1,16 @@
 class Ignore < Formula
-    desc "Manage your .gitignore file"
-    homepage "https://github.com/acevif/ignore"
-    url "https://github.com/acevif/ignore/archive/0.2.0.tar.gz"
-    sha256 "cddc75fc53a2a63ce634a46d8831403e080c3cb9865778651491c41bcb45bad0"
-    license "Unlicense"
-  
-    def install
-      bin.install "ignore"
-    end
-  
-    test do
-    end
+  version '0.2.1'
+
+  desc "Manage your .gitignore file"
+  homepage "https://github.com/acevif/ignore"
+  url "https://github.com/acevif/ignore/archive/0.2.1.tar.gz"
+  sha256 "9590b603e727b37b60ed4ac8a09418233bd8033d38d5aa5ec3a65895911365b9"
+  license "Unlicense"
+
+  def install
+    bin.install "ignore"
   end
-  
+
+  test do
+  end
+end
