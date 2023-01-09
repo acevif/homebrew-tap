452d4f881fe8e6f59e651c152f5d55ccb79d79c1
acevif <acevif@gmail.com>
2023-01-10 06:33:31 +0900

    Update

--- Formula/ignore.rb
+++ Formula/ignore.rb
@@ -6,6 +6,7 @@ class Ignore < Formula
   url "https://github.com/acevif/ignore/archive/0.2.2.tar.gz"
   sha256 "75bc00b57dfc120ce73a3bc2510f595feaad85df921b3f20c810fbf1a4119a5e"
   license "Unlicense"
+  depends_on "yq"
 
   def install
     bin.install "ignore"
