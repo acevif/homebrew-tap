7e816724cc5b7dad42eb36aea41e9c5aed28ad6a
acevif <acevif@gmail.com>
2023-01-10 06:27:20 +0900

    sha256

--- Formula/ignore.rb
+++ Formula/ignore.rb
@@ -4,6 +4,7 @@ class Ignore < Formula
   desc "Manage your .gitignore file"
   homepage "https://github.com/acevif/ignore"
   url "https://github.com/acevif/ignore/archive/0.2.2.tar.gz"
+  sha256 "75bc00b57dfc120ce73a3bc2510f595feaad85df921b3f20c810fbf1a4119a5e"
   license "Unlicense"
 
   def install
